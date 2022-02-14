a92c0d34e751173a193996906ca7d928dbb84499
SosnovGennadiy2006 <67744067+SosnovGennadiy2006@users.noreply.github.com>
2022-02-14 10:40:40 +0300

    Add empty MonoGame/WPF application

---
+++ MonoGameWPF_app/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Routing
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MonoGameWPF_app/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Routing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
